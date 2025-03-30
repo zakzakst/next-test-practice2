224b62b8ebd9cc9c902cd3e33be6e216fe0b7c7a
zakzakst <e1433104@gmai.com>
2025-03-30 13:03:53 +0900

    storybook x msw

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,21 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+import { handlers } from "../src/msw/handlers";
+
+initialize();
+
+export const decorators = [mswDecorator];
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
+    },
+    msw: {
+      handlers,
     },
   },
 };
